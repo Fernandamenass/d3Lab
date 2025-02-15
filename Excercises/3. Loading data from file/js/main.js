7f8b1155ca3ffba9ba0ba03c705f2b7707bec7ce
Fernandamenass <0243868@up.edu.mx>
2025-02-15 16:59:23 -0600

    Update main.js

--- Excercises/3. Loading data from file/js/main.js
+++ Excercises/3. Loading data from file/js/main.js
@@ -1,3 +1,56 @@
-d3.csv("data/ages.csv").then((data) => {
-  console.log(data);
-});
+/*
+ *    main.js
+ */
+
+d3.csv("data/ages.csv")
+  .then((data) => {
+    console.log("CSV Data Loaded:");
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error loading CSV:", error);
+  });
+
+d3.tsv("data/ages.tsv")
+  .then((data) => {
+    console.log("TSV Data Loaded:");
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error loading TSV:", error);
+  });
+
+d3.json("data/ages.json")
+  .then((data) => {
+    data.forEach((d) => {
+      d.age = +d.age;
+    });
+
+    const svg = d3
+      .select("#chart-area")
+      .append("svg")
+      .attr("width", 1000)
+      .attr("height", 400);
+
+    svg
+      .selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", (d, i) => i * 100 + 50)
+      .attr("cy", 100)
+      .attr("r", (d) => d.age * 4)
+      .attr("fill", (d) => {
+        if (d.age > 10) {
+          return "#0a9396";
+        } else {
+          return "#9b2226";
+        }
+      });
+
+    console.log(data);
+    console.log("Número de elementos:", data.length);
+  })
+  .catch((error) => {
+    console.error("Error loading JSON:", error);
+  });
