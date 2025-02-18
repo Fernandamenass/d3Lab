3164bd33248d32eb9699ba9116bf0ae47bf9f55a
Fernandamenass <0243868@up.edu.mx>
2025-02-17 18:24:38 -0600

    Reiniciar el 4, lo mezcle con el 5

--- Excercises/4. Scale/js/main.js
+++ Excercises/4. Scale/js/main.js
@@ -6,10 +6,8 @@ d3.json("data/buildings.json")
     console.log("Buildings Data Loaded:");
     console.log(data);
 
-    var margin = { left: 100, right: 10, top: 10, bottom: 100 };
-
-    var width = 600 - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
+    var width = 400;
+    var height = 400;
 
     var color = d3
       .scaleOrdinal()
@@ -19,23 +17,19 @@ d3.json("data/buildings.json")
     var x = d3
       .scaleBand()
       .domain(data.map((d) => d.name))
-      .range([0, 400]) //Ancho
+      .range([0, width])
       .paddingInner(0.3)
       .paddingOuter(0.3);
 
-    var y = d3.scaleLinear().domain([0, 828]).range([400, 0]); //Se invirtio para que primero salgan los mas altos
+    var y = d3.scaleLinear().domain([0, 828]).range([height, 0]); //Se invierte para que primero me salgan los mas altos
 
     var svg = d3
       .select("#chart-area")
       .append("svg")
-      .attr("width", 600)
-      .attr("height", 400);
+      .attr("width", 500)
+      .attr("height", 500);
 
-    var g = svg
-      .append("g") //En este grupo se aplica el margen
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-    var rects = g.selectAll("rect").data(data);
+    var rects = svg.selectAll("rect").data(data);
 
     rects
       .enter()
@@ -43,18 +37,8 @@ d3.json("data/buildings.json")
       .attr("x", (d) => x(d.name))
       .attr("y", (d) => y(d.height))
       .attr("width", x.bandwidth())
-      .attr("height", (d) => y(0) - y(d.height))
+      .attr("height", (d) => height - y(d.height))
       .attr("fill", (d) => color(d.name));
-
-    var xAxis = d3.axisBottom(x); //Eje inferior
-
-    g.append("g")
-      .attr("transform", `translate(0, ${height})`) // Coloca el eje en la parte inferior
-      .call(xAxis)
-      .selectAll("text") // Etiquetas del eje X
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-90)") // Rotación para evitar superposición
-      .style("font-size", "12px"); //Tamaño del texto
   })
   .catch((error) => {
     console.error("Error loading JSON:", error);
--- Excercises/5. Margins, groups, axes and labels/js/main.js
+++ Excercises/5. Margins, groups, axes and labels/js/main.js
@@ -6,6 +6,11 @@ d3.json("data/buildings.json")
     console.log("Buildings Data Loaded:");
     console.log(data);
 
+    var margin = { left: 100, right: 10, top: 10, bottom: 100 };
+
+    var width = 600 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+
     var color = d3
       .scaleOrdinal()
       .domain(data.map((d) => d.name))
@@ -23,12 +28,16 @@ d3.json("data/buildings.json")
     var svg = d3
       .select("#chart-area")
       .append("svg")
-      .attr("width", 500)
-      .attr("height", 500);
+      .attr("width", 600)
+      .attr("height", 400);
 
-    svg
-      .selectAll("rect")
-      .data(data)
+    var g = svg
+      .append("g") //En este grupo se aplica el margen
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    var rects = g.selectAll("rect").data(data);
+
+    rects
       .enter()
       .append("rect")
       .attr("x", (d) => x(d.name))
@@ -36,6 +45,16 @@ d3.json("data/buildings.json")
       .attr("width", x.bandwidth())
       .attr("height", (d) => y(0) - y(d.height))
       .attr("fill", (d) => color(d.name));
+
+    var xAxis = d3.axisBottom(x); //Eje inferior
+
+    g.append("g")
+      .attr("transform", `translate(0, ${height})`) // Coloca el eje en la parte inferior
+      .call(xAxis)
+      .selectAll("text") // Etiquetas del eje X
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-90)") // Rotación para evitar superposición
+      .style("font-size", "12px"); //Tamaño del texto
   })
   .catch((error) => {
     console.error("Error loading JSON:", error);
